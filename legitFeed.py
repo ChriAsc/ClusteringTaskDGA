a3bf4d8d521b34d89eace991aaf1e7b54f7a7444
lorenzo <s1107327@studenti.univpm.it>
2022-07-29 10:48:15 +0200

    aggiornato script legitFeed.py

--- legitFeed.py
+++ legitFeed.py
@@ -31,16 +31,16 @@ legit_feed = spark.createDataFrame([],schema)
 
 files = os.listdir(path)
 for file in files:
-    df = spark.read.format("csv").option("delimiter", ",").option("header", "true").load(f"{path}/{file}")
     if files.index(file) == 0:
-        legit_feed = legit_feed.union(df)
+        legit_feed = spark.read.format("csv").option("delimiter", ",").option("header", "true").load(f"{path}/{file}")
     else:
+        df = spark.read.format("csv").option("delimiter", ",").option("header", "true").load(f"{path}/{file}")
         legit_feed = legit_feed.union(df).dropDuplicates(["Domain"])
         # to_append_2 = legit_feed.union(df).groupBy("Domain").count()
 with open(f"{os.environ['HOME']}/Desktop/progettoBDA/Feed/legitFeed.csv", 'w') as filehandle:
+    filehandle.write(f"GlobalRank,TldRank,Domain,TLD,RefSubNets,RefIPs,IDN_Domain,IDN_TLD,PrevGlobalRank,PrevTldRank,PrevRefSubNets,PrevRefIPs\n")
     for riga in legit_feed.collect():
         row = ""
         for i in range(12):
-            if i != 11:
-                row += f"{riga[i]}," if i != 11 else f"{riga[i]}\n"
+            row += f"{riga[i]}," if i != 11 else f"{riga[i]}\n"
         filehandle.write(row)
