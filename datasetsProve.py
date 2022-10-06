48b4b563d00abac74e45744b30668815c83949a2
lorenzo <s1107327@studenti.univpm.it>
2022-10-06 14:18:23 +0200

    prima prova dataset

--- datasetsProve.py
+++ datasetsProve.py
@@ -22,28 +22,15 @@ schema = StructType([
     StructField("domain", StringType(), True),
 ])
 
-balanced_v1 = spark.createDataFrame([], schema)
-fasttext_v1 = spark.createDataFrame([], schema)
-dga_dataset_balanced = spark.createDataFrame([], schema)
-
-path = f"{os.environ['HOME']}/progettoBDA/FullyQualifiedDomains"
-classes = os.listdir(path)
-classes.sort()
-classes.remove('legit')
-
-# creating a balanced dataset of DGA domain names
-for family in classes:
-    df = spark.read.format("text").load(f"{path}/{family}/list/1000.txt")
-    labelled_domains = df.withColumns({"class": lit("dga"), "family": lit(family), "domain": df["value"]})
-    to_append = labelled_domains.select("class", "family", concat_ws('', split("domain", '[.]')).alias("noDotsDomain"), "domain")
-    dga_dataset_balanced = dga_dataset_balanced.union(to_append)
+#balanced_v1 = spark.createDataFrame([], schema)
+#fasttext_v1 = spark.createDataFrame([], schema)
+dga_dataset = spark.read.format("csv").option("delimiter", ",").option("header", "true").load(
+        f"{os.environ['HOME']}/progettoBDA/Feed/bambenekFeed.csv")
+dga_dataset = dga_dataset.withColumn("class",lit("dga")).withColumn("noDotsDomain", concat_ws('', split("domain", '[.]')))
 
 # creating two versions of balanced datasets with unigrams, bigrams and trigrams
-balanced_v1 = balanced_v1.union(dga_dataset_balanced.sample(0.002))
-fasttext_v1 = fasttext_v1.union(dga_dataset_balanced.subtract(balanced_v1).limit(5000))
-final_balanced_v1 = getNGrams(balanced_v1)
-final_fasttext_v1 = getNGrams(fasttext_v1)
+final_balanced_v1 = getNGrams(dga_dataset)
 
 # writing two datasets to two different csv files
-dataset_writer(f"{os.environ['HOME']}/progettoBDA/datasets/prova.csv", final_balanced_v1, mode='w')
-dataset_writer_fasttext(f"{os.environ['HOME']}/progettoBDA/datasets/", final_fasttext_v1)
+dataset_writer(f"{os.environ['HOME']}/progettoBDA/datasets/dataset3kk.csv", final_balanced_v1, mode='w')
+#dataset_writer_fasttext(f"{os.environ['HOME']}/progettoBDA/datasets/", final_fasttext_v1)
