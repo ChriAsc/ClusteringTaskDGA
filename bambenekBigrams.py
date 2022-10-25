c2a20f6c9c6289d8256c13f46675a8066748c1e5
lorenzo <s1107327@studenti.univpm.it>
2022-10-25 19:15:04 +0200

    Modifica clustering Task e bambenekBigrams

--- bambenekBigrams.py
+++ bambenekBigrams.py
@@ -1,4 +1,6 @@
 import os
+
+import getFamilies
 from datasetWriter import dataset_writer
 from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
@@ -26,7 +28,7 @@ schema = StructType([
 ])
 
 # raccolta e ordinamento in una lista di tutti i nomi dei file del bambenek feed
-bambenek_feeds = os.listdir(f"{os.environ['HOME']}/progettoBDA/DGAFeed/")
+bambenek_feeds = os.listdir(f"/media/lorenzo/Partizione Dati/progettoBDA/DGAFeed/")
 bambenek_feeds.sort()
 
 # creazione di un dataframe vuoto per ospitare i dati provenienti dai nuovi feed di bambenek
@@ -37,7 +39,7 @@ evil_feed = spark.createDataFrame([],schema)
 # mediante informazioni note e corrispondenze tra vari alias. Infine si ottiene  un dataframe che conterrà
 # tutti i domini da aggiungere al feed complessivo di cui riportiamo nome e famiglia
 for feed in bambenek_feeds:
-    df = spark.read.format('csv').load(f"{os.environ['HOME']}/progettoBDA/DGAFeed/{feed}", schema=schema)
+    df = spark.read.format('csv').load(f"/media/lorenzo/Partizione Dati/progettoBDA/DGAFeed/{feed}", schema=schema)
     evil_feed = evil_feed.union(df.filter(df['description'].isNotNull()))
 evil_feed = evil_feed.withColumn("family", split(evil_feed['link'], '[/.]')[6])
 evil_feed = evil_feed.withColumn("family", when(col('family') == 'cl', 'cryptolocker')
@@ -55,5 +57,13 @@ bigrams = NGram(n=2, inputCol="array_word", outputCol="bigrams")
 evil_bigrams = bigrams.transform(evil_feed)
 evil_bigrams = evil_bigrams.select("family",
                                     transform("bigrams",lambda x: concat_ws("", split(x, '[ ]'))).alias("bigrams"))
+fraction = 210000/evil_feed.count()
+evil_feed.createOrReplaceTempView("domains")
+family_with_count = sqlContext.sql("SELECT family, COUNT(family) as no_samples FROM domains GROUP BY family")
+family_with_count = family_with_count.withColumn("no_samples_modified", col("no_samples")*fraction)
+family_to_eliminate = family_with_count.filter("no_samples_modified < 1")
+family_to_eliminate = family_to_eliminate.rdd.flatMap(lambda x: x).collect()
 evil_bigrams = evil_bigrams.select("family",concat_ws(" ", filter("bigrams", is_bigram)).alias("bigrams"))
-dataset_writer(f"{os.environ['HOME']}/progettoBDA/datasets/bambenekBigrams.csv", evil_bigrams, mode='w')
+evil_bigrams = evil_bigrams.filter(~col("family").isin(family_to_eliminate))
+evil_bigrams_sampled = evil_bigrams.sample(210000/evil_feed.count())
+dataset_writer(f"/media/lorenzo/Partizione Dati/progettoBDA/datasets/bambenekBigrams.csv", evil_bigrams_sampled, mode='w')
--- clusteringTask.py
+++ clusteringTask.py
@@ -1,3 +1,4 @@
+import os.path
 import time
 
 import numpy as np
@@ -6,7 +7,7 @@ import fasttext
 from sklearn.cluster import DBSCAN
 from sklearn.neighbors import NearestNeighbors
 from kneed import KneeLocator
-from sklearn.metrics import homogeneity_score, completeness_score, v_measure_score
+from sklearn.metrics import homogeneity_score, completeness_score, v_measure_score, silhouette_score
 from sklearn.feature_extraction.text import CountVectorizer
 
 basepath_train_data = ""
@@ -72,76 +73,64 @@ def dist(v1, v2, **kwargs):
 def run(embedding_type="characters"):
     # NUMERI DA SCEGLIERE
     start = time.time()
-    columns = ["model", "epochs", "dimension", "homogeneity", "completeness",
-               "v_measure", "numClusters", "duration"]
+    columns = ["epochs", "dimension", "homogeneity", "completeness",
+               "v_measure", "silhouette_score", "num_clusters", "num_noise" "duration"]
     results = pd.DataFrame(columns=columns)
-    dataset = pd.read_csv('twoClassFullyBalanced.csv')  # NOME TEMPORANEO
+    dataset = pd.read_csv('bambenekBigrams.csv')  # NOME TEMPORANEO
     domain_names = dataset[embedding_type].to_numpy()
-    domain_names_for_vectorizing = dataset["noDotsDomain"].to_numpy()
+    # domain_names_for_vectorizing = dataset["noDotsDomain"].to_numpy()
     family_dict = {family: i for i, family in enumerate(sorted(set(dataset["family"])), 1)}
     labels_true = [family_dict[family] for family in dataset["family"].to_numpy()]
     max_len = np.max([len(x) for x in domain_names])
-    vectorizer = CountVectorizer(analyzer='char', ngram_range=embedding_params[embedding_type])
-    vectorizer.fit(domain_names_for_vectorizing)
-    word_index = vectorizer.vocabulary_
-    max_features = len(word_index)
-    for model_type in ["skipgram", "cbow"]:
-        for epoch in range(1, 11):
-            for dim in range(100, 301):
-                start_iteration = time.time()
-                embedding_matrix = np.zeros((max_features+1, dim))
-                model_skipgram = run_fasttext_training(basepath_train_data, model_type, dim, epoch, embedding_type)
-                dict_skipgram = getDict(model_skipgram)
-                for w, i in word_index.items():
-                    if dict_skipgram.get(w) is not None:
-                        embedding_matrix[i] = dict_skipgram[w]
-                embedded_domain_names = []
-                for name in domain_names:
-                    sequence_indexes = [word_index[token] for token in name.split()]
-                    pad = np.full(max_len-len(name.split()), max_features)
-                    embedded_domain_name = np.concatenate((sequence_indexes, pad), axis=None)
-                    embedded_domain_names.append(embedded_domain_name)
-                embedded_domain_names = np.array(embedded_domain_names)
-                # SCELTA PARAMETRI DBSCAN
-                # a rule of thumb is to derive minPts from the number of dimensions D in the data set. minPts >= D + 1.
-                # For larger datasets, with much noise, it suggested to go with minPts = 2 * D.
-                minPoints = dim+1
-                # cerco eps adatto
-                nn: NearestNeighbors = NearestNeighbors(n_neighbors=minPoints, metric=dist,
-                                                        metric_params={"embedding": embedding_matrix, "dim": dim})
-                nn.fit(embedded_domain_names)
-                distances = [x[-1] for x in nn.kneighbors(embedded_domain_names)[0]] # CONTIAMO ANCHE IL PUNTO STESSO COME PARTE DEL VICINATO?
-                distances.sort()
-                kneedle = KneeLocator(list(range(1, len(distances)+1)), distances, S=1.0, curve='convex', direction='increasing')
-                eps = round(kneedle.knee_y, 8)
-                # ESECUZIONE DBSCAN
-                #NELLA PAGINA DI SKLEARN DEL DBSCAN DICONO CHE PER RIDURRE LA COMPLESSITA':
-                #"One way to avoid the query complexity is to pre-compute sparse neighborhoods in chunks using
-                #NearestNeighbors.radius_neighbors_graph with mode='distance', then using metric='precomputed' here."
-                sparseMatrix = nn.radius_neighbors_graph(embedded_domain_names, mode="distance", sort_results=True)
-                #SE HO BEN CAPITO VA PASSATA LA MATRICE SOPRA SE PRECALCOLATA
-                db = DBSCAN(eps=eps, min_samples=minPoints, metric="precomputed")
-                db.fit(sparseMatrix)
-                # CONTROLLO PERFORMANCE RISPETTO AL PRECEDENTE RISULTATO MIGLIORE
-                labels = db.labels_
-                numClusters = len(set(labels)) - (1 if -1 in labels else 0)
-                numNoise = list(labels).count(-1)
-                #NON SONO SICURO SU QUALI METRICHE PRENDERE
-                homogeneity = homogeneity_score(labels_true, labels)
-                completeness = completeness_score(labels_true, labels)
-                v1_measure = v_measure_score(labels_true, labels)
-                end_iteration = time.time()
-                results = pd.concat([results,
-                                     pd.DataFrame([[model_type, epoch, dim, homogeneity, completeness,
-                                                    v1_measure, numClusters, end_iteration-start_iteration]],
-                                                  columns=columns)])
+    #vectorizer = CountVectorizer(analyzer='char', ngram_range=embedding_params[embedding_type])
+    #vectorizer.fit(domain_names_for_vectorizing)
+    #word_index = vectorizer.vocabulary_
+    #max_features = len(word_index)
+    for epoch in range(3, 8, 2):
+        for dim in range(8, 21, 3):
+            start_iteration = time.time()
+            #embedding_matrix = np.zeros((max_features+1, dim))
+            model_skipgram = run_fasttext_training(basepath_train_data, "skipgram", dim, epoch, embedding_type)
+            dict_skipgram = getDict(model_skipgram)
+            """for w, i in word_index.items():
+                if dict_skipgram.get(w) is not None:
+                    embedding_matrix[i] = dict_skipgram[w]"""
+            embedded_domain_names = []
+            for name in domain_names:
+                sequences = np.array(
+                    [dict_skipgram.get(token) if dict_skipgram.get(token) is not None else np.zeros(dim) for token in
+                     name.split()], dtype=np.single)
+                pad = np.zeros(dim * (max_len - len(name.split())), dtype=np.single)
+                embedded_domain_name = np.concatenate((sequences, pad), axis=None, dtype=np.single)
+                embedded_domain_names.append(embedded_domain_name)
+            # SCELTA PARAMETRI DBSCAN
+            # a rule of thumb is to derive minPts from the number of dimensions D in the data set. minPts >= D + 1.
+            # For larger datasets, with much noise, it suggested to go with minPts = 2 * D.
+            for minPoints in range((dim*max_len)+1, (2*dim*max_len)+1, (dim*max_len) // 5):
+                min_eps = max_eps = passo = 0
+                for eps in range(min_eps, max_eps, passo):
+                    #SE HO BEN CAPITO VA PASSATA LA MATRICE SOPRA SE PRECALCOLATA
+                    db = DBSCAN(eps=eps, min_samples=minPoints, metric="euclidean")
+                    db.fit(embedded_domain_names)
+                    # CONTROLLO PERFORMANCE RISPETTO AL PRECEDENTE RISULTATO MIGLIORE
+                    labels = db.labels_
+                    numClusters = len(set(labels)) - (1 if -1 in labels else 0)
+                    numNoise = list(labels).count(-1)
+                    #NON SONO SICURO SU QUALI METRICHE PRENDERE
+                    homogeneity = homogeneity_score(labels_true, labels)
+                    completeness = completeness_score(labels_true, labels)
+                    v1_measure = v_measure_score(labels_true, labels)
+                    silhouette = silhouette_score(embedded_domain_names, metric="euclidean")
+                    end_iteration = time.time()
+                    results = pd.DataFrame([[epoch, dim, homogeneity, completeness,
+                                             v1_measure, silhouette, numClusters, numNoise,
+                                             end_iteration-start_iteration]], columns=columns)
+                    results.to_csv(f"{embedding_type}_results.csv", index=False, mode='a',
+                                   header=not os.path.exists(f"{embedding_type}_results.csv"))
     end = time.time()
-    results.to_csv(f"{embedding_type}_results.csv", index=False)
     print(f"Took {end-start} seconds to perform task with {embedding_type} embedding")
 
 
 if __name__ == "__main__":
-    run()
     run("bigrams")
-    run("trigrams")
 
